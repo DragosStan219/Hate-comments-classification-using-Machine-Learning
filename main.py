0ad6600a3e6cfb55990577175172d8273b79d837
DragosStan219 <standragos72@gmail.com>
2025-11-17 17:19:25 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Load the required packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Load the datasets
+ds1 = pd.read_csv("train_dataset.csv")
+print(ds1) 
+
+clean_dataset = ds1.dropna()
+print(clean_dataset)
